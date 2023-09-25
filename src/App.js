961b1b4f40f05d3d65e807b387cf09836005fbea
itssophier <sofiajuma098@gmail.com>
2023-09-25 16:41:11 +0300

    Added categories and routing for products

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Category from './components/category';
 
 
 function App() {
-  const [results, setResults] = useState([])
+  const [categories, setCategories] = useState([])
+  const [products, setProducts] = useState([])
 
   //I have learnt how to use the useEffect hook to and the fetch API to read data from the json file and return a promise that logs the data to the console
   React.useEffect(() => {
@@ -12,23 +13,32 @@ function App() {
       .then(Response => Response.json())
       .then(data => {
         console.log(data)
-        setResults(data)
+        setCategories(data)
       })
   }, []) //never forget the array thing here because the console will keep looping and end up crashing our super duper nice server
 
+  //this is an onCLick handler for altering the rendered UIi component, this handles the category when clicked
+  const handleCategoryClick = id => {
+    fetch("http://localhost:3001/products?catId = " + id)
+      .then(Response => Response.json())
+      .then(data => {
+        console.log(data)
+        setProducts(data)
+      })
+  }
+
+
   const renderCategories = () => {
 
-  // basically, this is an alternative looping method compared to the .map method
-  
-  //   const categories = []
-  //   for (let i = 0; i < results.length; i++) {
-  //     categories.push(<Category key={results[i].id} id={results[i].id} title={results.title} />)
-  //   }
-  //   return categories
-  // }
-    return results.map( c => 
-      <Category key={c.id} id={c.id} title={c.title} />
+    return categories.map( c => 
+      <Category key={c.id} id={c.id} title={c.title} onCategoryClick = {() => handleCategoryClick(c.id)} />
     )
+  } 
+
+  const renderProducts = () => {
+    return products.map( p => 
+        <div>{p.title}</div>
+      )
   }
 
   //Here I have learnt how to use the map element to map through our json array, and the key which acts as a UID
@@ -39,11 +49,12 @@ function App() {
       <section>
         <nav>
           {
-           results && renderCategories()
+           categories && renderCategories()
           }
         </nav>
         <article>
-          main area
+         <h1>Products</h1>
+         {products && renderProducts()}
         </article>
       </section>
 
