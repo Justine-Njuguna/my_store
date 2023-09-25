f0accd5fe8d166dd55792c7bb94a9ac9cc9ee28e
itssophier <sofiajuma098@gmail.com>
2023-09-25 14:47:18 +0300

    Used the fetch API to 'fetch' data from our json file and rendered the data in the console

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [results, setResults] = useState([])
+
+  //I have learnt how to use the useEffect hook to and the fetch API to read data from the json file and return a promise that logs the data to the console
+  React.useEffect(() => {
+      fetch("http://localhost:3001/categories")
+      .then(Response => Response.json())
+      .then(data => {
+        console.log(data)
+        setResults(data)
+      })
+  }, []) //never forget the array thing here because the console will keep looping and end up crashing our super duper nice server
+
+  //Here I have learnt how to use the map element to map through our json array, and the key which acts as a UID
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {results.map(d => (
+        <div key={d.id}>{d.title}</div>
+      ))}
     </div>
   );
 }
