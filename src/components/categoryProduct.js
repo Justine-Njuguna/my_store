79e465bbd28cb1bdf3179b72c3a3288a042a59ff
Justine-Njuguna <hdenlysjustine@gmail.com>
2023-10-25 18:45:23 +0300

    Styled them components using the styled components library

--- src/components/categoryProduct.js
+++ src/components/categoryProduct.js
@@ -1,37 +1,59 @@
 import React from 'react'
-import '../App.css'
+import styled from 'styled-components'
 import { Link, useNavigate } from 'react-router-dom'
+
+const ProductTitle = styled.div`
+grid-column: 1 / span 3;
+color: darkslategray;
+font-weight: bold;
+font-size: 1.5rem;
+padding-left: 10px;
+`
+
+const ProductImageContainer = styled.div `
+    padding: 10px;
+    width: 60%;
+`
+
+const ProductImageContainerImage = styled.img `
+width: 474px;
+height: 355px;
+`
+
+const ProductInfo = styled.div `
+    display: flex;
+    flex-direction: column;
+`
 
 const CategoryProduct = ({id, title, image, specs, features, price, stock, dimensions}) => {
   const navigate = useNavigate()
     return (
-   <main>
     <article>
-    <div className='category-products-title'>
+    <ProductTitle>
         <Link to={`/products/${id}`}>{title}</Link>
-    </div>
+    </ProductTitle>
 
     <figure>
-        <div className='category-product-image-container'>
-            <img src={`/category-images/${image}`} alt='product_image' />
-        </div>
+        <ProductImageContainer>
+            <ProductImageContainerImage src={`/category-images/${image}`} alt='product_image' />
+        </ProductImageContainer>
     </figure>
 
     <aside className='category-product-details'>
-        <div className='category-product-info-dimensions'>
+        <ProductInfo>
             <h3>Dimensions</h3>
             <label>{ specs.dimensions}</label>
-        </div>
+        </ProductInfo>
 
         {specs.capacity &&
-        <div className='category-product-info-capacity'>
+        <ProductInfo>
             <h3>Capacity</h3>
             <label>{ specs.capacity}</label>
-        </div>
+        </ProductInfo>
     }
 
     <div className='category-product-info-features'>
-        <h3>features</h3>
+        <h3>Features</h3>
         <ul>
             {features?.map((f, i) => {
                 return <li key={`feature${i}`}>{f}</li>
@@ -40,9 +62,9 @@ const CategoryProduct = ({id, title, image, specs, features, price, stock, dimen
     </div>
     </aside>
 
-    <aside className='category-product-finance'>
-        <div className='category-product-finance-price'>
-            $pound;(price)
+    <aside className='category-product-info-finance'>
+        <div className='category-product-info-finance-price'>
+            &pound;{price}
         </div>
 
          <div className='category-product-info-stock'> 
@@ -56,7 +78,6 @@ const CategoryProduct = ({id, title, image, specs, features, price, stock, dimen
         </div>
     </aside>
  </article>
-   </main>
   )
 }
 
