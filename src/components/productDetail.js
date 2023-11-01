5d211408492b08d3068ded6db9afff5cbc9f5a0e
Justine-Njuguna <hdenlysjustine@gmail.com>
2023-11-01 19:07:51 +0300

    I am dying

--- src/components/productDetail.js
+++ src/components/productDetail.js
@@ -30,18 +30,6 @@ const ProductDetail = () => {
       </ProductImageContainer>
 
       <ProductInfo>
-        <ProductInfoHeader>Dimensions</ProductInfoHeader>
-        <label>{product.data.specs?.dimensions}</label>
-      </ProductInfo>
-
-      {product.data.specs?.capacity &&
-        <ProductInfo>
-          <ProductInfoHeader>Capacity</ProductInfoHeader>
-          <label>{product.data.specs?.capacity}</label>
-        </ProductInfo>
-      }
-
-      <ProductInfo>
         <ProductInfoHeader>Features</ProductInfoHeader>
         <ul>
           {product.data.features?.map((f, i) => (
@@ -70,81 +58,86 @@ const ProductDetail = () => {
 
 export default ProductDetail
 
-const ProductInfoArticle = styled.article`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 10px;
-  margin: 20px;
-`
-
-const ProductTitle = styled.div`
-  grid-column: 1 / span 3;
-  color: darkslategray;
-  font-weight: bold;
-  font-size: 1.5rem;
-  padding-left: 10px;
-`
-
-const ProductImageContainer = styled.div`
-  grid-column: 1 / span 3;
-  padding: 10px;
-  width: 100%;
-  text-align: center;
-`
-
-const ProductImage = styled.img`
-  max-width: 100%;
-  max-height: 300px;
-  object-fit: contain;
-`
-
 const ProductInfo = styled.div`
-  display: flex;
-  flex-direction: column;
+  margin-bottom: 0px;
 `
 
 const ProductInfoHeader = styled.h3`
   margin: 0;
 `
 
+const ProductInfoArticle = styled.article `
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 20px;
+    margin: 20px;
+    padding: 20px;
+    border: 1px solid #ccc;
+`;
+
+const ProductTitle = styled.div`
+    grid-column: span 3;
+    color: darkslategray;
+    font-weight: bold;
+    font-size: 1.5rem;
+    margin-bottom: 10px;
+`;
+
+const ProductImageContainer = styled.div`
+    grid-column: span 1;
+`;
+
+const ProductImage = styled.img`
+    max-width: 100%;
+    height: auto;
+`;
+
+const ProductInfoDetails = styled.div`
+    grid-column: span 1;
+`;
+
+const ProductInfoList = styled.ul`
+    list-style-type: disc;
+    padding-left: 20px;
+`;
+
 const ProductInfoListItem = styled.li`
-  list-style-type: disc;
-  margin-left: 20px;
-`
+    margin-bottom: 5px;
+`;
+
+const ProductInfoFinance = styled.div`
+    grid-column: span 1;
+`;
 
 const ProductInfoFinancePrice = styled.div`
-  grid-column: 1 / span 3;
-  font-size: 1.2rem;
-  text-align: center;
-`
+    font-weight: bold;
+    font-size: 1.2rem;
+    margin-bottom: 10px;
+`;
 
 const ProductInfoStock = styled.div`
-  grid-column: 1 / span 3;
-`
+    margin-bottom: 10px;
+`;
 
-const ProductInfoStockLabel = styled.label`
-  display: block;
-  margin-bottom: 5px;
-`
+const ProductInfoStockLabel = styled.div`
+    margin-bottom: 5px;
+`;
 
 const ProductInfoAction = styled.div`
-  grid-column: 1 / span 3;
-  text-align: center;
-`
+    display: flex;
+    justify-content: flex-start;
+`;
 
 const ProductInfoActionButton = styled.button`
-  background-color: #007bff;
-  color: #fff;
-  border: none;
-  padding: 10px 20px;
-  font-size: 1rem;
-  cursor: pointer;
-
-  &:hover {
-    background-color: #0056b3;
-  }
-`
+    padding: 10px 20px;
+    font-size: 1rem;
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+`;
 
 const ProductDescription = styled.div`
-  grid-column: 1 / span 3;
-`
+    grid-column: span 3;
+    margin-top: 20px;
+`;
